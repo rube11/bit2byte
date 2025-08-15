fb23d13becbbf659f0e4aa529576577de633cb7a
Robert Porchas <robertgeomaing@gmail.com>
2025-08-15 15:53:56 -0700

    This adds all of our current work into the repo still unfinished, but a good start I have yet to test it, so I left our old files in static_files

--- bittobyte/bittobyte/settings.py
+++ bittobyte/bittobyte/settings.py
@@ -15,7 +15,13 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+TEMPLATES[0]["DIRS"] = [BASE_DIR / "templates"]
+STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
+LOGIN_URL = "login"
+LOGIN_REDIRECT_URL = "home"      #login
+LOGOUT_REDIRECT_URL = "home"     #logout
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -37,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "accounts",
 ]
 
 MIDDLEWARE = [
