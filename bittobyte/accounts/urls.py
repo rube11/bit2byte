d410ba8fbcab6d890ef05fe1e00c2ea24809493e
Ruben <rubbythedubbey@gmail.com>
2025-08-19 14:38:54 -0700

    added urls to login page home and about page and tested to make sure everyhting was running and talking to eachother

--- bittobyte/accounts/urls.py
+++ bittobyte/accounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
     path("login/",  auth_views.LoginView.as_view(template_name="loginpage.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("signup/", views.signup, name="signup"),
