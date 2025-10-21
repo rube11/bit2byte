29a175cf480d861f9bc58891d6c57d5190a10430
rube11 <rubbythedubbey@gmail.com>
2025-10-21 16:55:32 -0700

    Started to create tests to establish CI/CD pipeline

--- bittobyte/accounts/tests.py
+++ bittobyte/accounts/tests.py
@@ -1,3 +1,5 @@
-from django.test import TestCase
-
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.core.exceptions import ValidationError
 # Create your tests here.
