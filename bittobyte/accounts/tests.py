6e8174659dbcbada55a4ff8d3e5a68747dee9196
rube11 <rubbythedubbey@gmail.com>
2025-10-22 09:20:14 -0700

    added gitignore and removing django key from commit history also modified requirements.txt

--- bittobyte/accounts/tests.py
+++ bittobyte/accounts/tests.py
@@ -1,5 +1,3 @@
 from django.test import TestCase, Client
-from django.contrib.auth.models import User
-from django.urls import reverse
-from django.core.exceptions import ValidationError
+
 # Create your tests here.
--- bittobyte/bittobyte/settings.py
+++ bittobyte/bittobyte/settings.py
@@ -11,13 +11,16 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
+load_dotenv
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'REMOVED'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
