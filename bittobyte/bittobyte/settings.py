c7f836de3a4fa09689947601ec09bbec6926a779
rube11 <rubbythedubbey@gmail.com>
2025-10-22 10:45:10 -0700

    added missing paranthesis to function

--- bittobyte/bittobyte/settings.py
+++ bittobyte/bittobyte/settings.py
@@ -18,7 +18,7 @@ from dotenv import load_dotenv
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-load_dotenv
+load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
