282bafaa1dfedb494bc6a71792f8b5739ddf7e9d
Ruben <rubbythedubbey@gmail.com>
2025-11-02 01:54:32 -0700

    setDebug to false for deployment

--- bittobyte/bittobyte/settings.py
+++ bittobyte/bittobyte/settings.py
@@ -23,7 +23,7 @@ load_dotenv()
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
